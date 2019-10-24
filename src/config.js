3a24a0b9a794371fcb4263018729ad107fe9ae7a
Yunfei He <i.heyunfei@gmail.com>
2019-10-24 21:28:58 +0800

    clear

--- src/config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { isDev } from './env';
-
-export function getRemoteHostUrl() {
-  if (isDev()) return 'http://localhost:3031/';
-  return 'http://101.37.174.138/';
-}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-
-// import * as serviceWorker from './serviceWorker';
-
-import Entry from './Entry';
-import './index.css';
-
-ReactDOM.render(React.createElement(Entry), document.getElementById('root'));
