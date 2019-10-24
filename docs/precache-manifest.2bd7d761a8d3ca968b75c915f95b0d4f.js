6308cd09d3565b20a6bbc083d3dbada151df4891
Yunfei He <i.heyunfei@gmail.com>
2019-10-24 22:02:44 +0800

    build docs

---
+++ docs/precache-manifest.2bd7d761a8d3ca968b75c915f95b0d4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da222f8fa74d418aef4084113c3b68c5",
+    "url": "/monica/index.html"
+  },
+  {
+    "revision": "bd3ef701c9a24705e6df",
+    "url": "/monica/static/css/main.33006cec.chunk.css"
+  },
+  {
+    "revision": "1b9f16f90645789773a5",
+    "url": "/monica/static/js/2.7f2d7d67.chunk.js"
+  },
+  {
+    "revision": "bd3ef701c9a24705e6df",
+    "url": "/monica/static/js/main.1e48cdd6.chunk.js"
+  },
+  {
+    "revision": "91953f8049370c7409fe",
+    "url": "/monica/static/js/runtime-main.4e78e802.js"
+  }
+]);
